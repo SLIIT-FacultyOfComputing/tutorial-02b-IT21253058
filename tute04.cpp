6024cbaa1d53fb281ac064a7720295167a9654eb
IT21253058 <99318195+IT21253058@users.noreply.github.com>
2022-02-25 07:19:25 +0000

    Tute04 completed

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,20 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+
+long nCr(int n, int r)
+{
+  return Factorial(n) / (Factorial(r) * Factorial(n - r));
+}
+
+long Factorial(int no)
+{
+  long facNo = 1;
+
+  for(int i = 1; i <= no; i++)
+    {
+      facNo = facNo * i;
+    }
+  
+  return facNo;
+}
